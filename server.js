8f1cb10de2765802799586c1572e8a517e173ee6
Mujib <ansarimujiburrehman@gmail.com>
2017-08-06 15:59:38 +0530

    Added
    Added details

--- server.js
+++ server.js
@@ -2,7 +2,7 @@
 * @Author: Mujib Ansari
 * @Date:   2017-07-22 22:15:39
 * @Last Modified by:   Mujib Ansari
-* @Last Modified time: 2017-07-29 02:49:37
+* @Last Modified time: 2017-08-06 15:25:06
 */
 
 'use strict';
@@ -19,14 +19,15 @@ var fs = require( 'fs' ),
 			baseUrl: 'https://en.wikipedia.org'
 		},
 		fileNames: {
-			allElmentList: 'files/allElmentList.json'
+			allElmentList: 'files/allElmentList.json',
+			alldetails: 'files/allDetails.json'
 		},
 
 
 		allElmentFile: null,
 
 		dummySuccessRes: { status: '200', response: 'Success.' },
-		gCount: 0
+		gCount: 481
 	};
 
 app.get( '/v1/api/list', function( p_req, p_res ) {
@@ -42,9 +43,12 @@ app.get( '/v1/api/list', function( p_req, p_res ) {
 } );
 
 app.get( '/v1/api/details', function( p_req, p_res ) {
-
+	console.log( '------------------- Started ----------------' );
 	scrappingDetails( p_res, function( p_response ) {
-		p_res.send( p_response );
+		// customWrite( oScope.fileNames.alldetails, JSON.stringify( p_response ), p_res, function() { 
+			p_res.send( p_response );
+		// });
+		
 	} )
 
 } );
@@ -150,9 +154,12 @@ function isValid( p_i ) {
 *====================================*
 =====================================*/
 function scrappingDetails( p_API_resp, p_fCallback ) {
-	console.log( p_fCallback );
+	
+
+	oScope.resJson = {};
+
 	if( oScope.allElmentFile ) {
-		recursiveScrap.call( oScope, function( p_reponse ) {
+		recursiveScrap.call( oScope, p_API_resp, function( p_reponse ) {
 			p_fCallback( p_reponse )
 		} );
 	}
@@ -160,7 +167,7 @@ function scrappingDetails( p_API_resp, p_fCallback ) {
 		customRead( oScope.fileNames.allElmentList, p_API_resp, function( p_fileData ) {
 			oScope.allElmentFile = JSON.parse( p_fileData );
 
-			recursiveScrap.call( oScope, function( p_reponse ) {
+			recursiveScrap.call( oScope, p_API_resp, function( p_reponse ) {
 				
 				p_fCallback( p_reponse )
 			} );
@@ -169,13 +176,13 @@ function scrappingDetails( p_API_resp, p_fCallback ) {
 
 };
 
-function recursiveScrap( p_fCallback ) {
+function recursiveScrap( p_API_resp, p_fCallback ) {
 
 	var nLen = this.allElmentFile.length,
-		currentElem = this.allElmentFile[ this.gCount ];
+		currentElem = this.allElmentFile[ this.gCount ],
+		sKeyName = Math.floor( Math.random()*10000 );
 
-	console.log( nLen, currentElem 	);
-
+	oScope.resJson = {};
 	request( this.url.baseUrl + currentElem.link, function( p_err, p_htmlRes, p_html ) {
 
 		if( p_err )
@@ -191,7 +198,9 @@ function recursiveScrap( p_fCallback ) {
 		respJson.name = $( '#firstHeading' ).text().trim();
 		respJson.desc = removeRefrences( $( '#mw-content-text' ).find( 'p' ).eq( 0 ) );
 		respJson.images = [];
+		respJson.link = currentElem.link;
 
+		console.log( 'Scarpping count :: ' + oScope.gCount + '/' + nLen + '. Current :: ' + respJson.name );
 		for( ; i < allTr.length - 3; i++ ) {
 
 			if( $( allTr[ i ] ).find( 'th' ).length == 0 ) {
@@ -207,7 +216,9 @@ function recursiveScrap( p_fCallback ) {
 							alt: $( image ).attr( 'alt' ),
 							src: $( image ).attr( 'src' )
 						}
-						respJson.images.push( imgObj );
+
+						if( imgObj.alt.toLowerCase() != 'yes' )
+							respJson.images.push( imgObj );
 					} else {
 						/*
 						*--- Assume there is no Image
@@ -225,7 +236,7 @@ function recursiveScrap( p_fCallback ) {
 
 
 						if( $( $( allTr[ i ] ).find( 'div' ) ).find( 'a' ) )
-							console.log( '999999999999999999999999999' );
+							// console.log( 'Despacito :: ' + $( $( allTr[ i ] ).find( 'div' ) ).find( 'a' ).text() );
 
 
 						respJson[ aValue ] = divValue;
@@ -244,22 +255,47 @@ function recursiveScrap( p_fCallback ) {
 						var sKey = $( allTr[ i ] ).find( 'td' ).eq( 0 ).text().trim(),
 							sValue = $( allTr[ i ] ).find( 'td' ).eq( 1 ).text().trim();
 
+						if( sKey.toLowerCase() == 'chemical formula' || sKey.toLowerCase() == 'density' )
+							sValue = $( allTr[ i ] ).find( 'td' ).eq( 1 ).html().trim();
 						respJson[ sKey ] = sValue;
+
+						if( sKey.toLowerCase() == 'chemical formula' )
+							sKeyName = $( allTr[ i ] ).find( 'td' ).eq( 1 ).text().trim();
 					}
 					
 
 				}
 
-
-
 			}
 
 		}
 
-		console.log( '==================================' );
-		console.log( respJson );
-		if( p_fCallback )
-			p_fCallback( respJson );
+		// oScope.resJson[ sKeyName ] =  respJson;
+		
+		if( oScope.gCount <= nLen ) {
+			
+
+			// customRead( oScope.fileNames.alldetails, p_API_resp, function( p_fileData ) {
+
+				// p_fileData = JSON.parse( p_fileData );
+				// console.log( '--- --- --- --- --- --- --- --- --- --- --- ---' );
+				// console.log( p_fileData );
+				// p_fileData[ sKeyName ] =  respJson;
+				console.log( 'Writting ' + respJson.name + '.json' + ' size is :: ' + Object.keys( respJson ).length );
+				customWrite( 'files/' + respJson.name + '.json', respJson, p_API_resp, function() { 
+					oScope.gCount++;
+					recursiveScrap.call( oScope, p_API_resp, p_fCallback );
+				});
+			// });
+				
+			
+		} else {
+			if( p_fCallback ) {
+				console.log( oScope.resJson )
+				p_fCallback( oScope.resJson );	
+			}
+		}
+		
 	} );
 
 
